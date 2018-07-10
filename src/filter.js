f4816f7652403d35ad2d1a112a9e6b1bc6bbfc62
Joshua Soong <joshua@soong.co>
2018-07-11 09:50:44 +1200

    experimental gif support

--- src/filter.js
+++ src/filter.js
@@ -5,7 +5,7 @@ function filterDomains(listing) {
   var filtered = [];
   for (i=0; i<listing.length; i++)
     for (j=0; j<IMAGE_DOMAINS.length; j++)
-      if (listing[i].data.domain===IMAGE_DOMAINS[j]&&(listing[i].data.url.endsWith(".jpg")||listing[i].data.url.endsWith(".jpeg")||listing[i].data.url.endsWith(".png"))) {
+      if (listing[i].data.domain===IMAGE_DOMAINS[j]&&(listing[i].data.url.endsWith(".jpg")||listing[i].data.url.endsWith(".jpeg")||listing[i].data.url.endsWith(".png")||listing[i].data.url.endsWith(".gif"))) {
         filtered.push(listing[i]);
         continue;
       }
